da5b4a89f005f50becd7e15e338de2edb9a4aed5
Miha Bondarenko <limon18@ukr.net>
2022-09-20 00:29:32 +0300

    add

---
+++ src/js/01-gallery.js
@@ -0,0 +1,59 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+galleryRef.addEventListener('click', onImgClick);
+
+const addItemGallery = galleryItems.map(
+  ({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+      <a href="${original}" class="gallery__link">
+        <img src="${preview}" alt="${description}" class="gallery__image">
+      </a>
+    </div>`;
+  }
+);
+const markup = addItemGallery.join('');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
+//
+//---------------------------------------------
+import SimpleLightbox from 'simplelightbox';
+//---------------------------------------------
+import 'simplelightbox/dist/simple-lightbox.min.css';
+//---------------------------------------------
+function onImgClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 400,
+  showCounter: true,
+  enableKeyboard: true,
+  docClose: true,
+  scrollZoom: true,
+  animationSlide: true,
+  maxZoom: 1.5,
+});
+//
+//
+//
+//todo Завдання 1 - бібліотека SimpleLightbox
+//? Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+//! Додай бібліотеку SimpleLightbox як залежність проекту, використовуючи npm (посилання на CDN з твоєї минулої роботи більше не потрібне).
+//
+//! Використовуй свій JavaScript код з попередньої домашньої роботи, але виконай рефакторинг з урахуванням того, що бібліотека була встановлена через npm (синтаксис import/export).
+//
+//! Для того щоб підключити CSS код бібліотеки в проект, необхідно додати ще один імпорт, крім того, що описаний в документації.
+
+//? Описаний в документації
+//* import SimpleLightbox from "simplelightbox";
+//? Додатковий імпорт стилів
+//* import "simplelightbox/dist/simple-lightbox.min.css";
---
+++ src/js/02-video.js
@@ -0,0 +1,49 @@
+// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
+
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+// ---------------------------------------
+player.on('timeupdate', function (data) {
+  //
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+});
+const timeLocalStr = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+// console.log(timeLocalStr);
+// ---------------------------------------
+player.getPlayed().then(function (played) {
+  played.push(timeLocalStr);
+  console.log(played);
+});
+//todo Завдання 2 - відеоплеєр
+//? HTML містить <iframe> з відео для Vimeo плеєра. Напиши скрипт, який буде зберігати поточний час відтворення відео у локальне сховище і, після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.
+
+//* <iframe
+//*   id="vimeo-player"
+//*   src="https://player.vimeo.com/video/236203659"
+//*   width="640"
+//*   height="360"
+//*   frameborder="0"
+//*   allowfullscreen
+//*   allow="autoplay; encrypted-media"
+//* ></iframe>
+
+//! Виконуй це завдання у файлах 02-video.html і 02-video.js. Розбий його на декілька підзавдань:
+//
+//! Ознайомся з документацією бібліотеки Vimeo плеєра.
+//
+//! Додай бібліотеку як залежність проекту через npm.
+//
+//! Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+//
+//! Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+//
+//! Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+//
+//! Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
+//
+//! Додай до проекту бібілотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
