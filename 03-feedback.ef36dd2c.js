e52eb04decb6002e48001bc8950980e9ecdcad5c
miha-bond <79281448+miha-bond@users.noreply.github.com>
2022-09-21 21:40:33 +0000

    Deploying to gh-pages from @ miha-bond/goit-js-hw-08@ee1acb6259ae41a5fdd58546007bf0d77680f5cb 🚀

--- 03-feedback.ef36dd2c.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector(".feedback-form"),document.querySelector("input"),document.querySelector("textarea"),document.querySelector("button")][1].addEventListener("input",(function(e){var t=e.target.value;console.log(t)}));
-//# sourceMappingURL=03-feedback.ef36dd2c.js.map
