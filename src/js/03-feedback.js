e6892db1cdca5d60a9c96b5052fc07116da6fb90
Miha Bondarenko <limon18@ukr.net>
2022-09-22 12:01:39 +0300

    add Task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,44 @@
 import throttle from 'lodash.throttle';
 import { load, save, remove } from './storage';
-// ----------------------------------
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  email: document.querySelector('input'),
-  message: document.querySelector('textarea'),
-  button: document.querySelector('button'),
-};
-// -----------------------------------
-const inoutVelue = event => {
-  const { value } = event.target;
-  console.log(value);
-};
-refs.email.addEventListener('input', inoutVelue);
+// +++++++++++++++++++++++++++++++++++++++++++
+const LOCALSTORAGE_FORM_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+// +++++++++++++++++++++++++++++++++++++++++++
+initPage();
 
-// ---------------------------------
+const onFormInput = event => {
+  const { name, value } = event.target;
+  let saveData = load(LOCALSTORAGE_FORM_KEY);
+  saveData = saveData ? saveData : {};
+  saveData[name] = value;
+  save(LOCALSTORAGE_FORM_KEY, saveData);
+};
+
+const throttleOnFormInput = throttle(onFormInput, 500);
+formRef.addEventListener('input', throttleOnFormInput);
+// -------------------------------------------
+function initPage() {
+  const saveData = load(LOCALSTORAGE_FORM_KEY);
+  if (!saveData) {
+    return;
+  }
+  Object.entries(saveData).forEach(([name, value]) => {
+    formRef.elements[name].value = value;
+  });
+}
+// -------------------------------------------
+const onSubmit = event => {
+  event.preventDefault();
+  const {
+    elements: { name, email, message },
+  } = event.currentTarget;
+  event.currentTarget.reset();
+  remove(LOCALSTORAGE_FORM_KEY);
+};
+
+formRef.addEventListener('submit', onSubmit);
+// -------------------------------------------
+// ===========================================
 //todo Завдання 3 - форма зворотного зв'язку
 //? HTML містить розмітку форми. Напиши скрипт, який буде зберігати значення полів у локальне сховище, коли користувач щось друкує.
 //
